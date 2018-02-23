0c65245e63ba40d08f73c0c08ac7e67c4f79017b
paultretiak <pavel.t@advantagemedia.agency>
2018-02-23 14:52:37 +0100

    fix views

--- src/main/java/web/repository/LinkRepository.java
+++ src/main/java/web/repository/LinkRepository.java
@@ -4,9 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import web.model.CSP;
 import web.model.Link;
 
+import java.util.List;
 import java.util.Set;
 
 public interface LinkRepository extends JpaRepository<Link, Integer> {
     Link getByUrl(String url);
-    Set<Link> findAllByCspOrderByIdDesc(CSP csp);
+    List<Link> findAllByCspOrderByIdDesc(CSP csp);
 }
